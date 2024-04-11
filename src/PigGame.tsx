f9008c966dd8b40ab2abcf73c85b991902953914
Dani <danichilling@gmail.com>
2024-04-11 21:23:34 +0100

    added in player two score & determining which player's turn it is

--- src/PigGame.tsx
+++ src/PigGame.tsx
@@ -5,6 +5,8 @@ export default function PigGame() {
   const [lastRoll, setLastRoll] = useState<number | null>(null);
   const [runningTotal, setRunningTotal] = useState<number>(0);
   const [playerOneScore, setplayerOneScore] = useState<number>(0);
+  const [playerTwoScore, setplayerTwoScore] = useState<number>(0);
+  const [playerNumber, setPlayerNumber] = useState<number>(1);
 
   function rollDie() {
     let rollResult = sample([1, 2, 3, 4, 5, 6]);
@@ -13,6 +15,7 @@ export default function PigGame() {
       setRunningTotal(rollResult + runningTotal);
     } else {
       setRunningTotal(0);
+      togglePlayer();
       //toggle whose turn it is
     }
   }
@@ -21,21 +24,31 @@ export default function PigGame() {
     // setPotatoes(prev => [...prev, 10])
     // setplayerOneScore(x => x + runningTotal);
 
-    setplayerOneScore(playerOneScore + runningTotal);
+    if (playerNumber % 2 == 0) {
+      setplayerTwoScore(playerTwoScore + runningTotal);
+    } else {
+      setplayerOneScore(playerOneScore + runningTotal);
+    }
+    togglePlayer();
     setRunningTotal(0);
+  }
+
+  function togglePlayer() {
+    setPlayerNumber((x) => x + 1);
   }
 
   return (
     <div className="gameDisplay">
       <div className="gameTitle">Pig Game 🐖</div>
-      <div className="item">Player One Score: {playerOneScore}</div>
-      <div className="item">Player Two Score: {playerOneScore}</div>
-
-      <div className="item">
+      <div className="score-box">
+        <br />
+        <div>Player One Score: {playerOneScore}</div>
+        <div>Player Two Score: {playerTwoScore}</div>
+        <br />
         <center>
-          <div className="item">
-            Turn Total: {runningTotal} | Last Roll:{" "}
-            {lastRoll === null ? "-" : lastRoll}
+          <div>
+            Last Roll: {lastRoll === null ? "-" : lastRoll} | Turn Total:{" "}
+            {runningTotal}
           </div>
         </center>
         <button className="rollButton" onClick={rollDie}>
@@ -44,6 +57,9 @@ export default function PigGame() {
         <button className="holdButton" onClick={holdScore}>
           hold
         </button>
+      </div>
+      <div className="player-turn">
+        {playerNumber % 2 == 0 ? "Player Two's Turn" : "Player One's Turn"}
       </div>
     </div>
   );
